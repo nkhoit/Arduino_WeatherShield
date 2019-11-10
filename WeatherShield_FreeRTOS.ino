b6df8d7ebd9169262b64c3dc329f2946b7b2a1a8
Khoi Tran <nkhoi.tran@gmail.com>
2019-11-10 11:20:30 -0500

    Updated include paths. Added boilerplate tasks. Added doc for weatherShield

--- WeatherShield_FreeRTOS.ino
+++ WeatherShield_FreeRTOS.ino
@@ -1,12 +1,192 @@
+#include <TEWeatherShield.h>
 #include <Arduino_FreeRTOS.h>
+#include <semphr.h>
 
+void TaskHTU21D(void *pvParameters);
+void TaskMS5637(void *pvParameters);
+void TaskMS8607(void *pvParameters);
+void TaskTSD305(void *pvParameters);
 void TaskTSY01(void *pvParameters);
 
-void setup() {
-  // put your setup code here, to run once:
+// Mutex lock for i2c bus
+SemaphoreHandle_t gSensorMtx = NULL;
 
+// weatherShield object
+static TEWeatherShield gWeatherShield;
+
+// Order: HTU21D, MS5637, MS8607, TSD305, TSY01
+static float gTemperature[5];
+
+void setup() {
+  // Initialize Serial bus
+  Serial.begin(115200);
+
+  // Initialize weather shield
+  gWeatherShield.begin();
+
+  while (!Serial) {
+    // Going to wait here until we connect the serial terminal
+  }
+
+  xTaskCreate(
+    TaskHTU21D,
+    (const portCHAR *) "HTU21D",
+    128,
+    NULL,
+    1,
+    NULL
+  );
+
+  xTaskCreate(
+    TaskMS5637,
+    (const portCHAR *) "MS5637",
+    128,
+    NULL,
+    1,
+    NULL
+  );
+
+  xTaskCreate(
+    TaskMS8607,
+    (const portCHAR *) "MS8607",
+    128,
+    NULL,
+    1,
+    NULL
+  );
+
+  xTaskCreate(
+    TaskTSD305,
+    (const portCHAR *) "TSD305",
+    128,
+    NULL,
+    1,
+    NULL
+  );
+
+  xTaskCreate(
+    TaskTSY01,
+    (const portCHAR *) "TSY01",
+    128,
+    NULL,
+    1,
+    NULL
+  );
 }
 
 void loop() {
   // Nothing happens here
+}
+
+void TaskHTU21D(void *pvParameters)
+{
+  float temperature, humidity;
+
+  for(;;)
+  {
+    // Check for mutex
+    if(xSemaphoreTake(gSensorMtx, (TickType_t) 10) == pdTRUE) {
+      // Select HTU21D on multiplexer
+      gWeatherShield.selectHTU21D();
+
+      // Make sure this sensor is actually connected
+      if (gWeatherShield.HTU21D.is_connected()) {
+        gWeatherShield.HTU21D.set_i2c_master_mode(htu21_i2c_no_hold);
+        gWeatherShield.HTU21D.read_temperature_and_relative_humidity(&temperature, &humidity);
+
+        gTemperature[0] = temperature;
+      }
+
+      // Release mutex
+      xSemaphoreGive(gSensorMtx);
+    }
+  }
+}
+
+void TaskMS5637(void *pvParameters)
+{
+  float temperature, pressure;
+
+  for(;;)
+  {
+    // Check for mutex
+    if(xSemaphoreTake(gSensorMtx, (TickType_t) 10) == pdTRUE) {
+      // Select MS5637 on multiplexer
+      gWeatherShield.selectMS5637();
+
+      // Make sure this sensor is actually connected
+      if (gWeatherShield.MS5637.is_connected()) {
+        gWeatherShield.MS5637.read_temperature_and_pressure(&temperature, &pressure);
+
+        gTemperature[1] = temperature;
+      }
+
+      // Release mutex
+      xSemaphoreGive(gSensorMtx);
+    }
+  }
+}
+
+void TaskMS8607(void *pvParameters)
+{
+  float temperature, pressure, humidity;
+
+  for(;;)
+  {
+    if(xSemaphoreTake(gSensorMtx, (TickType_t) 10) == pdTRUE) {
+      gWeatherShield.selectMS8607();
+
+      // Make sure this sensor is actually connected
+      if (gWeatherShield.MS8607.is_connected()) {
+        gWeatherShield.MS8607.set_humidity_i2c_master_mode(ms8607_i2c_hold);
+        gWeatherShield.MS8607.read_temperature_pressure_humidity(&temperature, &pressure, &humidity);
+
+        gTemperature[2] = temperature;
+      }
+
+      xSemaphoreGive(gSensorMtx);
+    }
+  }
+}
+
+void TaskTSD305(void *pvParameters)
+{
+  float temperature, object_temperature;
+
+  for(;;)
+  {
+    if(xSemaphoreTake(gSensorMtx, (TickType_t) 10) == pdTRUE) {
+      gWeatherShield.selectTSD305();
+
+      // Make sure this sensor is actually connected
+      if (gWeatherShield.TSD305.is_connected()) {
+        gWeatherShield.TSD305.read_temperature_and_object_temperature(&temperature, &object_temperature);
+
+        gTemperature[3] = temperature;
+      }
+
+      xSemaphoreGive(gSensorMtx);
+    }
+  }
+}
+
+void TaskTSY01(void *pvParameters)
+{
+  float temperature;
+
+  for(;;)
+  {
+    if(xSemaphoreTake(gSensorMtx, (TickType_t) 10) == pdTRUE) {
+      gWeatherShield.selectTSYS01();
+
+      // Make sure this sensor is actually connected
+      if (gWeatherShield.TSYS01.is_connected()) {
+        gWeatherShield.TSYS01.read_temperature(&temperature);
+
+        gTemperature[4] = temperature;
+      }
+
+      xSemaphoreGive(gSensorMtx);
+    }
+  }
 }
